e719fbbcb6fc58f137acd6913f0e50e62634dd4b
iroarish <isaac.mcvill.up@gmail.com>
2025-03-13 20:51:10 +0800

    ito promise

--- manage-appointment.php
+++ manage-appointment.php
@@ -35,7 +35,7 @@
     </script>
     <script>
         function ViewDetails(id) {
-            var form = $('<form action="appointment-details.php" method="POST">' + '<input type="hidden" name="appointment_id" id="appointment_id" value="'+id+'">' + '</form>');
+            var form = $('<form action="manage-page.php" method="POST">' + '<input type="hidden" name="appointment_id" id="appointment_id" value="'+id+'">' + '</form>');
             //$.post("database-connection/login.php", {'employee_id' : appointment_id, 'password': 'password'}, function(){window.location.href='database-connection/login.php'});
             $('body').append(form);
             form.submit();
